8b122f0f0dd59e139bed3ec0070d21630cb3c433
sillyangel <sillyangel3@gmail.com>
2022-11-14 17:20:31 +0000

    added Blocks

--- src/main/java/com/sillyangel/commandmod/init/BlockInit.java
+++ src/main/java/com/sillyangel/commandmod/init/BlockInit.java
@@ -1,14 +1,12 @@
-package com.cy4.tutorialmod.init;
+package com.sillyangel.commandmod.init;
 
-import java.util.function.Function;
+import java.unit.function.Function;
 
 import com.sillyangel.commandmod.Commandmod;
 import com.google.common.base.Supplier;
 
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.level.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
@@ -17,25 +15,21 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class BlockInit {
-	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
-			TutorialMod.MOD_ID);
-	public static final DeferredRegister<Item> ITEMS = ItemInit.ITEMS;
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
+            Commandmod.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = ItemInit.ITEMS;
 
-	public static final RegistryObject<Block> EXAMPLE_BLOCK = register("example_block",
-			() -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_PURPLE).strength(3.0f)
-					.sound(SoundType.METAL).requiresCorrectToolForDrops()),
-			object -> () -> new BlockItem(object.get(), new Item.Properties().tab(TutorialMod.TUTORIAL_TAB)));
-	
+    public static final RegistryObject<Block> EXAMPLE_BLOCK = register("example_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAl, MaterialColor.COLOR_PURPLE)), null);
 
-	private static <T extends Block> RegistryObject<T> registerBlock(final String name,
-			final Supplier<? extends T> block) {
+    private static <T extends Block> RegistryObject<T> registerBlock(final String name, 
+            final Supplier<? extends T> block) {
 		return BLOCKS.register(name, block);
 	}
-
-	private static <T extends Block> RegistryObject<T> register(final String name, final Supplier<? extends T> block,
-			Function<RegistryObject<T>, Supplier<? extends Item>> item) {
+    private static <T extends Block> RegistryObject<T> register(final String name, final Supplier<? extends T> block,
+            Function<RegistryObject<T>, Supplier<? extends Item>> item) {
 		RegistryObject<T> obj = registerBlock(name, block);
-		ITEMS.register(name, item.apply(obj));
-		return obj;
+        ITEMS.register(name, item.apply(obj));
+        return obj;
 	}
 }
